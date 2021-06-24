cd89726e3ca8ddfd2aaa978e68b116daaf66bfdc
obourligueux2021 <nolive4@free.fr>
2021-06-24 17:31:30 +0200

    Commit 17h30 Olivier

--- src/dal/impl/ArticleImpl.java
+++ src/dal/impl/ArticleImpl.java
@@ -1,50 +1,32 @@
 package dal.impl;
 
 import bo.Article;
+import bo.Utilisateur;
 import dal.ConnectionProvider;
+import dal.FactoriesDao;
 import dal.IGenericDao;
 import exception.GlobalException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ArticleImpl implements IGenericDao<Article> {
 
 
-    private static final String SQL_SELECT_ALL_ARTICLES = "SELECT article, prix_vente, date_fin_encheres FROM ARTICLES";
+    private static final String SQL_SELECT_ALL_ARTICLES = "SELECT article, prix_vente, date_fin_encheres, no_utilisateur FROM ARTICLES";
 
-    private static final String SQL_SELECT_BY_ARTICLE = "SELECT u.no_utilisateur, u.pseudo, a.no_article, a.article, a.prix_vente, a.date_fin_encheres" +
-            "FROM UTILISATEURS u INNER JOIN ARTICLES a ON u.no_categorie = a.no_article";
+    private static final String SQL_SELECT_BY_ID = "SELECT article, prix_vente, date_fin_encheres, no_utilisateur FROM ARTICLES WHERE id = ?";
 
-    private static final String SQL_SELECT_BY_ID = "SELECT * FROM ARTICLES WHERE id=?";
-
-    private static final String SQL_SELECT_ARTICLES_BY_CRITERES = "SELECT u.no_utilisateur, u.pseudo, a.no_article, a.article, a.prix_vente, a.date_fin_encheres, c.no_categorie, c.libelle" +
-            "FROM UTILISATEURS u INNER JOIN ARTICLES a ON u.no_utlisateur = a.no_article" +
-            "INNER JOIN CATEGORIES c ON a.no_article = c.no_categorie";
 
     @Override
-    public void insert(Article obj) throws GlobalException {
-
-    }
+    public void insert(Article obj) throws GlobalException {}
 
     @Override
-    public void update(Article obj) throws GlobalException {
-
-    }
+    public void update(Article obj) throws GlobalException {}
 
     @Override
-    public void delete(int id) throws GlobalException {
-
-    }
-
-    @Override
-    public Article selectById(int id) throws GlobalException {
-        return null;
-    }
+    public void delete(int id) throws GlobalException {}
 
     @Override
     public List<Article> selectAll() throws GlobalException {
@@ -56,13 +38,15 @@ public class ArticleImpl implements IGenericDao<Article> {
         ) {
             PreparedStatement pstt = con.prepareCall(SQL_SELECT_ALL_ARTICLES);
             ResultSet rs = pstt.executeQuery();
-
             while (rs.next()) {
                 //Je choisis les paramètres de l'objet avec le get
                 Article artAjout = new Article();
                 artAjout.setArticle(rs.getString("article"));
                 artAjout.setPrixVente(rs.getInt("prix_vente"));
                 artAjout.setDateFin(rs.getDate("date_fin_encheres").toLocalDate().atTime(0, 0));
+                //J'ajoute l'item "Vendeur"
+                Utilisateur ut = FactoriesDao.getUtilisateurDao().selectById(rs.getInt("no_utilisateur"));
+                artAjout.setUtilisateur(ut);
                 //J'acjoute l'article à la liste
                 list.add(artAjout);
             }
@@ -73,82 +57,97 @@ public class ArticleImpl implements IGenericDao<Article> {
         return list;
     }
 
-//    @SneakyThrows
-//    @Override
-//    public Article selectByArticle(String article) throws GlobalException {
-//        Article a = new Article();
-//        try (
-//                Connection con = ConnectionProvider.getConnection()
-//        ) {
-//            PreparedStatement pstt3 = con.prepareCall(SQL_SELECT_BY_ARTICLE);
-//            ResultSet rs = pstt3.executeQuery();
-//
-//            //On choisit les paramètres de son objet avec le get
+    @Override
+    public Article selectById(int id) throws GlobalException {
+        //Je crée un article
+        Article art = new Article();
+        //Je lance la connexion
+        try (
+                Connection con = ConnectionProvider.getConnection()
+        ) {
+            PreparedStatement pstt = con.prepareCall(SQL_SELECT_BY_ID);
+            ResultSet rs = pstt.executeQuery();
+            while (rs.next()) {
+                //Je choisis les paramètres de l'objet avec le get
+                art.setArticle(rs.getString("article"));
+                art.setPrixVente(rs.getInt("prix-vente"));
+                art.setDateFin(rs.getDate("date_fin_encheres").toLocalDate().atTime(0, 0));
+                //J'ajoute l'item "Vendeur"
+                Utilisateur ut = FactoriesDao.getUtilisateurDao().selectById(rs.getInt("no_utilisateur"));
+                art.setUtilisateur(ut);
+                pstt.executeQuery();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        //Je renvoie l'article
+        return art;
+    }
+
+    @Override
+    public List<Article> selectByCriteres(String articleName, String catName, boolean openedEnchere,
+                                          boolean inprogressEnchere, boolean winEnchere,
+                                          boolean inprogressVente, boolean beforeVente, boolean finishedVente) throws GlobalException {
+
+        String SQL_SELECT_ARTICLES_BY_CRITERES = "SELECT a.no_categorie, a.article, a.prix_vente, a.date_fin_encheres, c.no_categorie, c.libelle " +
+                "FROM ARTICLES a INNER JOIN CATEGORIES c ON a.no_categorie = c.no_categorie WHERE";
+
+        //Je crée une liste
+        List<Article> list = new ArrayList<Article>();
+        //Je lance la connexion
+        try (
+                Connection con = ConnectionProvider.getConnection()
+        ) {
+            PreparedStatement pstt = con.prepareCall(String.valueOf(SQL_SELECT_ARTICLES_BY_CRITERES));
+
+            //Je trie en fonction du choix utilisateur
+            StringBuilder sqlConstruction = new StringBuilder(SQL_SELECT_ARTICLES_BY_CRITERES);
+            //Texte libre
+            if("nomtext" != null) {
+                sqlConstruction.append(" articleName = nomtext");
+            }
+
+            //Choix catégorie
+            if("categorie" == "toutes") { sqlConstruction.append(", catName = toutes");}
+            else if("categorie" == "informatique") { sqlConstruction.append(", catName = informatique");}
+            else if("categorie" == "ameublement") { sqlConstruction.append(", catName = ameublement");}
+            else if("categorie" == "vetement") { sqlConstruction.append(", catName = vetement");}
+            else {sqlConstruction.append(", catName = sports&loisirs");};
+
+            //Choix des checkbox
+            if(openedEnchere) {
+                sqlConstruction.append(", openedEnchere == true"); }
+            if(inprogressEnchere) {
+                sqlConstruction.append(", inprogressEnchere == true"); }
+            if(winEnchere) {
+                sqlConstruction.append(", winEnchere == true"); }
+            if(inprogressVente) {
+                sqlConstruction.append(", inprogressVente == true"); }
+            if(beforeVente) {
+                sqlConstruction.append(", beforeVente == true"); }
+            if(finishedVente) {
+                sqlConstruction.append(", finishedVente == true"); }
+            System.out.println(sqlConstruction);
+            ResultSet rs = pstt.executeQuery();
+
 //            while (rs.next()) {
-//                a.setArticle(rs.getString("article"));
-//                //a.setDateFin(rs.getDate("dateFin".toLocalDateTime()));
-//                a.setPrixVente(rs.getInt("prixVente"));
-//                //a.setUtilisateur(rs.);
-//                pstt3.executeQuery();
+//                //Je choisis les paramètres de l'objet avec le get
+//                Article artAjout = new Article();
+//                artAjout.setArticle(rs.getString("article"));
+//                artAjout.setPrixVente(rs.getInt("prix_vente"));
+//                artAjout.setDateFin(rs.getDate("date_fin_encheres").toLocalDate().atTime(0, 0));
+//                //J'ajoute l'item "Vendeur"
+//                Utilisateur ut = FactoriesDao.getUtilisateurDao().selectById(rs.getInt("no_utilisateur"));
+//                artAjout.setUtilisateur(ut);
+//                }
+//                //J'acjoute l'article à la liste
+//                //list.add(artAjout);
 //            }
-//        }
-//        try {
-//            return IGenericDao.super.selectByArticle(article);
-//        } catch (GlobalException e) {
-//            e.printStackTrace();
-//        }
-//        return a;
-//    }
-//
-//    @Override
-//    public Article selectById(int id) throws GlobalException {
-//        List<Article> list = new ArrayList<>();
-//
-//        try (
-//                Connection con = ConnectionProvider.getConnection()
-//        ) {
-//            PreparedStatement pstt = con.prepareCall(SQL_SELECT_BY_ID);
-//            ResultSet rs = pstt.executeQuery();
-//            while (rs.next()) {
-//
-//                //On choisit les paramètres de son objet avec le get
-//                list.getUtilisateur();
-//                list.getCategorie();
-//                list.setArticle(rs.getString("article"));
-//                //a.setDateFin(rs.getDate("dateFin".toLocalDateTime()));
-//                list.setPrixVente(rs.getInt("prixVente"));
-//                pstt.executeQuery();*/
-//            }
-//        }
-//        //return IGenericDao.super.selectByCriteres(list<Article>);
-//        return list;
-//    }
-//
-//    @SneakyThrows
-//    @Override
-//    public List<Article> selectByCriteres(String articleName, String catName, boolean openedEnchere,
-//                                          boolean inprogressEnchere, boolean winEnchere,
-//                                          boolean inprogressVente, boolean beforeVente, boolean finishedVente) throws GlobalException {
-//        List<Article> list = new ArrayList<>();
-//
-//        try (
-//                Connection con = ConnectionProvider.getConnection()
-//        ) {
-//            PreparedStatement pstt = con.prepareCall(SQL_SELECT_ARTICLES_BY_CRITERES);
-//            ResultSet rs = pstt.executeQuery();
-//            while (rs.next()) {
-//
-///*                //On choisit les paramètres de son objet avec le get
-//                list.getUtilisateur();
-//                list.getCategorie();
-//                list.setArticle(rs.getString("article"));
-//                //a.setDateFin(rs.getDate("dateFin".toLocalDateTime()));
-//                list.setPrixVente(rs.getInt("prixVente"));
-//                pstt.executeQuery();*/
-//            }
-//        }
-//        //return IGenericDao.super.selectByCriteres(list<Article>);
-//        return list;
-//    }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return list;
+    }
 
 }
