8bb257f346c90e54c0d543018b82ee091ad0b2a7
captGouZ <julien.gazeaurousseau@gmail.com>
2021-07-01 11:38:15 +0200

    Test enchère

--- src/exception/testClass.java
+++ src/exception/testClass.java
@@ -1,34 +1,6 @@
 package exception;
 public class testClass {
-// A FAIRE    ce soir et demain matin
-// TODO : requête du montant dispo ne prend pas en compte les articles retirés : A tester
-// TODO_ok : tester la suppression utilisateur : validé Olivier + Julien
-// TODO_ok : Vérifier l'heure de début et fin d'enchère : validé Alex
-// TODO_ok : Changer la servlet accueilS en accueil : validé Olivier
-/* TODO : Vérification des exception. Le header contien "messageErreur" et "messageConfirm" pour les affichages
-    Utilisateur
-    Dal
-        Article -> Olivier : ok : tous les champs obligatoires déclenchent une exception dans Nouvelle Vente
-        Enchere -> Julien : ok
-        Adresse -> validé Julien
-        Categorie -> Olivier : ok
-        User -> validé Julien
-    Managers
-        AdresseManager -> validé Julien
-        ArticleManager -> Olivier : ok
-        CategorieManager -> Olivier : ok
-        ConnexionManager -> Alex
-        EnchereManager -> Julien
-        UserManager -> validé Julien
-    Servlet
-        GestionCompte -> validé Julien
-        AccueilServlet -> Julien
-        AfficherEnchere -> reste à tester le retrait
-        Connexion -> validé Julien
-        Vente->Alex
-
-  */
-
+// TODO : Créer un jeu d'essai pour tester chaque check box
 // SECONDAIRE    demain après-midi
 // TODO : CSS / JS sur les option bouton : Olivier
 // TODO : Message confirmation et erreur à afficher dans les JSP : Julien
